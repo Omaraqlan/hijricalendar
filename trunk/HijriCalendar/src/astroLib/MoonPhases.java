9ac7dd09c92f9580892c321de31443a2befbc1a6
cepmuvakkit <cepmuvakkit@e44d01da-369f-11de-b310-9106b1f7c239>
2009-08-02 13:27:42 +0000

    Correction of Hijri Year

---
+++ trunk/HijriCalendar/src/astroLib/MoonPhases.java
@@ -0,0 +1,16 @@
+package astroLib;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author mgeden
+ */
+public abstract class MoonPhases {
+  public  abstract double calculatePhase(double T);
+}
